f82e6615f744bd62d13d80db7f1ed4132f6fc8e3
dir-FY24kotakitakamasa <takamasa.kotaki.v702@dirbato.co.jp>
2024-04-05 17:15:48 +0900

    04051715マデ

---
+++ Sample/src/sample/ExceptionExample1.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class ExceptionExample1 {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        System.out.println("処理スタート");
+        
+        try {
+            System.out.println("割り算開始");
+            int a = 10 / 0;
+            System.out.println(a);
+            
+        }catch(ArithmeticException e) {
+            System.out.println("ArithmeticExceptionが発生: " + e.getMessage());
+        }finally {
+            System.out.println("割り算終了");
+            
+        }
+        
+        System.out.println("処理終了");
+    }
+
+}
